39d906b173fc29a4cb8ca2121d94e493cdff7769
Jenson-Bolton <jensonbolton@outlook.com>
2021-11-04 22:34:24 +0000

    Lexer started

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-class Lexer {
-  constructor() {}
+var input = document.getElementById('input');
+var output = document.getElementById('output');
+
+var stream = input.value;
+var tokenizer = new Tokenizer();
+
+addEventListener('keyup', () => {
+    stream = input.value;
+
+});
+
+class Tokenizer {
+    constructor() {
+        this.string = stream;
+        this.cursor = 0;
+    }
+
+    updateString() {
+        this.string = stream;
+    }
+
+    hasMoreTokens() {
+        return this.cursor < this.string.length;
+    }
+
+    getNextToken() {
+
+    }
 }
