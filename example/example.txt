85d3be8ac33e1f04adf5f5333baec5d07162067f
Jenson Bolton <69874214+Jenson-Bolton@users.noreply.github.com>
2021-11-05 14:03:54 +0000

    Parser setup and supporting some syntax

---
+++ example/example.txt
@@ -0,0 +1,30 @@
+# Welcome to NoteMD
+
+Hi there this is **NoteMD.** *Made* by **[JensonBolton](jensonbolton.com)**.
+
+> This is my blockquote
+> > This is my blockquote in a blockquote
+
+# Heading Level 1
+## Heading Level 2
+### Heading Level 3
+#### Heading Level 4
+##### Heading Level 5
+###### Heading Level 6
+
+1. First item
+2. Second item
+3. Third item
+   1. Indented item
+   2. Indented item
+4. Fourth item
+
+- First item
+- Second item
+- Third item
+  - Indented item 
+  - Indented item
+- Fourth item
+
+![The San Juan Mountains are beautiful!](https://mdg.imgix.net/assets/images/san-juan-mountains.jpg?auto=format&fit=clip&q=40&w=1080 "San Juan Mountains")
+[![An old rock in the desert](https://mdg.imgix.net/assets/images/shiprock.jpg?auto=format&fit=clip&q=40&w=1080 "Shiprock, New Mexico by Beau Rogers")](https://www.flickr.com/photos/beaurogers/31833779864/in/photolist-Qv3rFw-34mt9F-a9Cmfy-5Ha3Zi-9msKdv-o3hgjr-hWpUte-4WMsJ1-KUQ8N-deshUb-vssBD-6CQci6-8AFCiD-zsJWT-nNfsgB-dPDwZJ-bn9JGn-5HtSXY-6CUhAL-a4UTXB-ugPum-KUPSo-fBLNm-6CUmpy-4WMsc9-8a7D3T-83KJev-6CQ2bK-nNusHJ-a78rQH-nw3NvT-7aq2qf-8wwBso-3nNceh-ugSKP-4mh4kh-bbeeqH-a7biME-q3PtTf-brFpgb-cg38zw-bXMZc-nJPELD-f58Lmo-bXMYG-bz8AAi-bxNtNT-bXMYi-bXMY6-bXMYv)
--- js/script.js
+++ js/script.js
@@ -1,29 +1,43 @@
-var input = document.getElementById('input');
-var output = document.getElementById('output');
+var input = document.getElementById("input");
+var output = document.getElementById("output");
 
-var stream = input.value;
-var tokenizer = new Tokenizer();
+var markdown = input.value;
 
-addEventListener('keyup', () => {
-    stream = input.value;
+function parseMarkdown(markdownText) {
+  const htmlText = markdownText
+    .replace(/^###### (.*$)/gim, '<h6 class="heading-6">$1</h6>')
+    .replace(/^##### (.*$)/gim, '<h5 class="heading-5">$1</h5>')
+    .replace(/^#### (.*$)/gim, '<h4 class="heading-4">$1</h4>')
+    .replace(/^### (.*$)/gim, '<h3 class="heading-3">$1</h3>')
+    .replace(/^## (.*$)/gim, '<h2 class="heading-2">$1</h2>')
+    .replace(/^# (.*$)/gim, '<h1 class="heading-1">$1</h1><hr />')
+    .replace(/^\> (.*$)/gim, '<blockquote class="md">$1</blockquote>')
+    .replace(/\*\*(.*)\*\*/gim, '<span class="bold">$1</span>')
+    .replace(/\*(.*)\*/gim, '<span class="italic">$1</span>')
+    .replace(/!\[(.*?)\]\((.*?)\)/gim, '<img class="img" alt="$1" src="$2" />')
+    .replace(/\[(.*?)\]\((.*?)\)/gim, '<a class="link" href="$2">$1</a>')
+    .replace(/\n$|  /gim, "<br />");
 
+  return htmlText.trim();
+}
+
+input.addEventListener("keyup", () => {
+  markdown = input.value;
+  output.innerHTML = parseMarkdown(markdown);
 });
 
-class Tokenizer {
-    constructor() {
-        this.string = stream;
-        this.cursor = 0;
-    }
+// Make tabs typeable
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Tab") {
+    e.preventDefault();
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
 
-    updateString() {
-        this.string = stream;
-    }
+    // set textarea value to: text before caret + tab + text after caret
+    this.value =
+      this.value.substring(0, start) + "\t" + this.value.substring(end);
 
-    hasMoreTokens() {
-        return this.cursor < this.string.length;
-    }
-
-    getNextToken() {
-
-    }
-}
+    // put caret at right position again
+    this.selectionStart = this.selectionEnd = start + 1;
+  }
+});
